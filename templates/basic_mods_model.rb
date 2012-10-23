0452c8a56a6724fe4ec55e9199e58e103b03eba5
Monty Hindman <montyhindman@gmail.com>
2012-10-23 09:50:20 -0700

    Modified edit view to agree with Record model

--- templates/basic_mods_model.rb
+++ templates/basic_mods_model.rb
@@ -1,13 +1,12 @@
 class Record < ActiveFedora::Base
 
-  has_metadata :name => "descMetadata", :type => ModsDescMetadata
+  has_metadata :name => 'descMetadata', :type => ModsDescMetadata
 
-  delegate :title, :to=>'descMetadata', :at => [:mods, :titleInfo, :title], :unique=>true
-  delegate :abstract, :to => "descMetadata"
-  delegate :preferred_citation, :to => "descMetadata", :unique=>true
-  delegate :author, :to=>'descMetadata', :at => [:name, :namePart], :unique=>true
-  delegate :url, :to=>'descMetadata', :at => [:relatedItem, :location, :url], :unique=>true
-  delegate :description, :to=>'descMetadata', :at => [:abstract], :unique=>true
+  delegate :title,              :to => 'descMetadata', :unique=>true, :at => [:mods, :titleInfo, :title]
+  delegate :author,             :to => 'descMetadata', :unique=>true, :at => [:name, :namePart]
+  delegate :abstract,           :to => 'descMetadata', :unique=>true
+  delegate :preferred_citation, :to => 'descMetadata', :unique=>true
+  delegate :related_url,        :to => 'descMetadata', :unique=>true, :at => [:relatedItem, :location, :url]
 
 end
 
